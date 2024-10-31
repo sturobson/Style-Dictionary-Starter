a0d2ef3e58d63ce58ea2a455e7e37da6e7a8cb32
Stuart Robson <services@sturobson.com>
2024-10-31 11:44:38 +0000

    updates build file

--- build.js
+++ build.js
@@ -21,6 +21,63 @@ const myStyleDictionary = new StyleDictionary({
           destination: '_variables.scss',
           format: 'scss/variables',
         },
+        {
+          destination: 'tokens-map.scss',
+          format: 'scss/map-flat',
+        },
+        {
+          destination: 'tokens-map-deep.scss',
+          format: 'scss/map-deep',
+        },
+      ],
+    },
+    js: {
+      transformGroup: 'js',
+      buildPath: 'build/js/',
+      files: [
+        {
+          destination: 'tokens.js',
+          format: 'javascript/object',
+        },
+        {
+          destination: 'tokens-flat.js',
+          format: 'javascript/module-flat',
+        },
+        {
+          destination: 'tokens-umd.js',
+          format: 'javascript/umd',
+        },
+      ],
+    },
+    ts: {
+      transformGroup: 'js',
+      buildPath: 'build/types/',
+      files: [
+        {
+          destination: 'tokens.d.ts',
+          format: 'typescript/es6-declarations',
+        },
+      ],
+    },
+    android: {
+      transformGroup: 'android',
+      buildPath: 'build/android/',
+      files: [
+        {
+          destination: 'values/tokens.xml',
+          format: 'android/resources',
+        },
+      ],
+    },
+    ios: {
+      transformGroup: 'ios-swift',
+      buildPath: 'build/ios/',
+      files: [
+        {
+          destination: 'Tokens.swift',
+          format: 'ios-swift/class.swift',
+          className: 'Tokens', // Name of the Swift class
+        },
       ],
     },
   },
