a822dc47ac4cde099ed80df643602448d65f4980
Stuart Robson <services@sturobson.com>
2024-12-06 13:51:53 +0000

    part 5 code

--- build.js
+++ build.js
@@ -1,57 +1,26 @@
+import fs from 'fs';
 import StyleDictionary from 'style-dictionary';
-import chroma from 'chroma-js';
 
-// Register additional transforms
-StyleDictionary.registerTransform({
-  name: 'color/hexToDisplayP3',
-  type: 'value',
-  filter: (token) => token.$type === 'color',
-  transform: (token) => {
-    const rgb = chroma(token.$value).rgb(); // Possible issue here
-    return `color(display-p3 ${rgb[0] / 255} ${rgb[1] / 255} ${rgb[2] / 255})`;
-  }
-});
+// Read all .tokens files from the 'src/tokens' directory
+const tokenFiles = fs.readdirSync('src/tokens').filter((file) => file.endsWith('.tokens'));
 
 // Configure Style Dictionary
 const myStyleDictionary = new StyleDictionary({
-  source: [`src/tokens/**/*.tokens`],
+  source: tokenFiles.map((file) => `src/tokens/${file}`),
   platforms: {
-    css: {
-      transformGroup: 'css',
-      buildPath: 'build/css/',
-      files: [
-        {
-          destination: 'all.css',
-          format: 'css/variables',
-        },
-      ],
-    },
-    android: {
-      transformGroup: 'android',
-      buildPath: 'build/android/',
-      files: [
-        {
-          destination: 'all.xml',
-          format: 'android/resources',
-        },
-      ],
-    },
-    ios: {
-      transformGroup: 'ios-swift',
-      buildPath: 'build/ios/',
-      files: [
-        {
-          destination: 'all.swift',
-          format: 'ios-swift/class.swift',
-        },
-      ],
+    scss: {
+      transformGroup: 'scss',
+      buildPath: 'build/scss/',
+      files: tokenFiles.map((file) => ({
+        destination: `${file.replace('.tokens', '.scss')}`, // Replace '.tokens' with '.scss' for the destination
+        format: 'scss/variables',
+        filter: (token) => file === token.filePath.split('/').pop(), // Match tokens by filename
+      })),
     },
   },
   hooks: {
     transformGroups: {
-      css: ['attribute/cti', 'name/kebab', 'color/hexToDisplayP3'],
-      android: ['attribute/cti', 'name/snake', 'size/remToSp', 'color/hex8android'],
-      ios: ['attribute/cti', 'name/pascal', 'size/swift/remToCGFloat', 'color/UIColorSwift'],
+      scss: ['size/pxToRem'],
     },
   },
 });
