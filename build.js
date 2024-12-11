20b803c0c2e6faf63e4f4169d5ccba1afa7f1960
Stuart Robson <services@sturobson.com>
2024-12-11 11:49:04 +0000

    updates build script

--- build.js
+++ build.js
@@ -1,26 +1,27 @@
-import fs from 'fs';
 import StyleDictionary from 'style-dictionary';
-
-// Read all .tokens files from the 'src/tokens' directory
-const tokenFiles = fs.readdirSync('src/tokens').filter((file) => file.endsWith('.tokens'));
 
 // Configure Style Dictionary
 const myStyleDictionary = new StyleDictionary({
-  source: tokenFiles.map((file) => `src/tokens/${file}`),
+  source: ['src/tokens/**/*.tokens'],
+
   platforms: {
-    scss: {
-      transformGroup: 'scss',
-      buildPath: 'build/scss/',
-      files: tokenFiles.map((file) => ({
-        destination: `${file.replace('.tokens', '.scss')}`, // Replace '.tokens' with '.scss' for the destination
-        format: 'scss/variables',
-        filter: (token) => file === token.filePath.split('/').pop(), // Match tokens by filename
-      })),
-    },
-  },
-  hooks: {
-    transformGroups: {
-      scss: ['size/pxToRem'],
+    css: {
+      transformGroup: 'css',
+      // Resolve references to generate hard values
+      resolveReferences: true,
+      buildPath: 'build/css/',
+      files: [
+        {
+          destination: 'base/tokens.css', // Output file for base tokens
+          format: 'css/variables',
+          filter: (token) => token.filePath.includes('base'), // Filter for base tokens
+        },
+        {
+          destination: 'semantic/tokens.css', // Output file for semantic tokens
+          format: 'css/variables',
+          filter: (token) => token.filePath.includes('semantic'), // Filter for semantic tokens
+        },
+      ],
     },
   },
 });
